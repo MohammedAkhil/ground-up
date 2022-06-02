87e6409f79365e392828c93a683930826c26dfac
Akhil <akhilmohammed05@gmail.com>
2022-06-02 18:56:51 +0530

    Add scripts

---
+++ install.sh
@@ -0,0 +1,114 @@
+
+
+echo "Creating an SSH key for you..."
+ssh-keygen -t rsa
+
+echo "Please add this public key to Github \n"
+echo "https://github.com/account/ssh \n"
+read -p "Press [Enter] key after this..."
+
+echo "Installing xcode-stuff"
+xcode-select --install
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update homebrew recipes
+echo "Updating homebrew..."
+brew update
+
+echo "Installing Git..."
+brew install git
+
+echo "Git config"
+
+git config --global user.name "Akhil Mohammed"
+git config --global user.email akhilmohammed05@gmail.com
+
+brew install ${cat packages.txt}
+
+# brew install github/gh/gh
+
+#Install Zsh & Oh My Zsh
+echo "Installing Oh My ZSH..."
+curl -L http://install.ohmyz.sh | sh
+
+apps=(
+  spotify
+  google-chrome
+  maccy
+  iterm2
+  brave-browser
+  1password
+  enpass
+  authy
+  fig
+  hammerspoon
+  karabiner-elements
+  obsidian 
+  notion
+  ticktick
+  visual-studio-code
+  tableplus
+  robo-3t
+  postman
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps with Cask..."
+brew install --appdir="/Applications" ${apps[@]}
+
+
+brew cleanup
+
+#"Setting trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+
+#"Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+#"Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+#"Use column view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+#"Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 36
+
+
+#"Enabling the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+#Show Hidden Files in Finder
+defaults write com.apple.finder AppleShowAllFiles YES
+
+defaults write com.apple.finder ShowPathbar -bool true
+
+defaults write com.apple.finder ShowStatusBar -bool true
+
+
+
+git clone https://github.com/powerline/fonts.git
+cd fonts
+sh -c ./install.sh
+cd ../
+
+
+
+
+
+killall Finder
+
+
+echo "Done!"
+
